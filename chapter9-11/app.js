10d447f1fce0b869f9d11422a5b4990f6cacb5a5
Abdul Raziq <154526495+Abdu-raziq@users.noreply.github.com>
2024-03-31 18:11:21 +0500

    Add files via upload

--- chapter9-11/app.js
+++ chapter9-11/app.js
@@ -92,29 +92,120 @@
 // }
 
 // Question no 6
-let total_marks=+prompt("Enter total marks");
-let obtain_marks=+prompt("Enter obtained marks");
-let percentage =obtain_marks/total_marks*100;
-document.write("<h1>Marks Sheet</h1>");
-document.write(`Total marks : ${total_marks} <br>`); 
-document.write(`Marks obtained: ${obtain_marks} <br>`);
-document.write(`Percentage : ${percentage}% <br>`);  
-if(percentage>=80){
-    document.write(`Grade : A-one <br>`);
-    document.write(`Remarks : Excellent <br>`);
+// let total_marks=+prompt("Enter total marks");
+// let obtain_marks=+prompt("Enter obtained marks");
+// let percentage =obtain_marks/total_marks*100;
+// document.write("<h1>Marks Sheet</h1>");
+// document.write(`Total marks : ${total_marks} <br>`); 
+// document.write(`Marks obtained: ${obtain_marks} <br>`);
+// document.write(`Percentage : ${percentage}% <br>`);  
+// if(percentage>=80){
+//     document.write(`Grade : A-one <br>`);
+//     document.write(`Remarks : Excellent <br>`);
+// }
+// else if(percentage>=70){
+//     document.write(`Grade : A <br>`);
+//     document.write(`Remarks : Good <br>`);
+// }
+// else if(percentage>=60){
+//     document.write(`Grade : B <br>`);
+//     document.write(`Remarks : You need to improve <br>`);
+// }
+// else if(percentage<60){
+//     document.write(`Grade : Fail <br>`);
+//     document.write(`Remarks : Sorry!!! <br>`);
+// }
+// else{
+//     Swal.fire("Invalid!!!");
+// }
+
+// Question no 7
+// let secret_no=8;
+// let guess=+prompt("Guess the secret number");
+// if(guess===8){
+//     Swal.fire("Bingo! Correct answer");
+// }
+// else if(guess === 8+1){
+//     Swal.fire("Close enough to the correct answer");
+// }
+// else{
+//     Swal.fire("No match!!!");
+// }
+
+// Question no 8
+// let number = +prompt("Enter the number:");
+// if(number % 3 === 0){
+//     Swal.fire("The number is divisible by 3!!!");
+// }
+// else{
+//     Swal.fire("The number is not divisible by 3!!!");
+// }
+
+// Question no 9
+// let even_odd=+prompt("Enter the number");
+// if(even_odd % 2===0){
+//     Swal.fire(`${even_odd} is even number!!!`);
+// }
+// else{
+//     Swal.fire(`${even_odd} is odd number!!!`);
+// }
+
+// Question no 10
+// let temperature=+prompt("Enter the temperature");
+// if(temperature>40){
+//     Swal.fire("Its too hot outside !!!");
+// }
+// else if(temperature>30){
+//     Swal.fire("Today wheather is normal !!!");
+// }
+// else if(temperature>20){
+//     Swal.fire("Today's wheather is cool !!!");
+// }
+// else if(temperature>10){
+//     Swal.fire("OMG!!! Today's wheather is so cool !!!");
+// }
+// else if(temperature<10){
+//     Swal.fire(`OMG!!! ${temperature}C<sup>0</sup>  is so cool !!!`);
+// }
+// else{
+//     Swal.fire("Invalid!!!");
+// }
+
+let user_input=+prompt("Calculator \n\n 1.+ \n 2.- \n 3.* \n 4./ \n 5.% \n\n Enter your choice");
+if(user_input ===1){
+    let add1=+prompt("Enter first number");
+    let add2=+prompt("Enter second number");
+    let add = add1 + add2 ;
+    Swal.fire(`Addition of ${add1} + ${add2} = ${add}`);
 }
-else if(percentage>=70){
-    document.write(`Grade : A <br>`);
-    document.write(`Remarks : Good <br>`);
+else if(user_input ===2){
+    let sub1=+prompt("Enter first number");
+    let sub2=+prompt("Enter second number");
+    let sub = sub1 - sub2 ;
+    Swal.fire(`Subtraction of ${sub1} - ${sub2} = ${sub}`);
 }
-else if(percentage>=60){
-    document.write(`Grade : B <br>`);
-    document.write(`Remarks : You need to improve <br>`);
+else if(user_input ===3){
+    let mul1=+prompt("Enter first number");
+    let mul2=+prompt("Enter second number");
+    let mul = mul1 * mul2 ;
+    Swal.fire(`Multiplication of ${mul1} * ${mul2} = ${mul}`);
 }
-else if(percentage<60){
-    document.write(`Grade : Fail <br>`);
-    document.write(`Remarks : Sorry!!! <br>`);
+else if(user_input ===4){
+    let div1=+prompt("Enter first number");
+    let div2=+prompt("Enter second number");
+    let div = div1 / div2 ;
+    Swal.fire(`Division of ${div1} / ${div2} = ${div}`);
 }
+else if(user_input ===5){
+    let rem1=+prompt("Enter first number");
+    let rem2=+prompt("Enter second number");
+    let rem = rem1 % rem2 ;
+    Swal.fire(`Reminder of ${rem1} / ${rem2} = ${rem}`);
+}
+else{
+    Swal.fire("Invalid please try again!!!");
+}
+
 
 
 
